d94ae9d88676517f9a68b4f98693da241aee4daa
Big.Daddy.Brooks <slbrooks81@gmail.com>
2017-08-31 09:06:13 -0500

    lab 1 and notes

---
+++ 3333_BrooksS_Lab01/3333_BrooksS_Lab01/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Stephen Brooks
+000571395
+8/31/2017
+9/05/2017
+A sad, empty, program shell
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3333_BrooksS_Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
